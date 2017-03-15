f311c0e9f23d2838bb7078953784d248fbfae507
battlequinn <quinnthebattle@gmail.com>
2017-03-15 11:38:51 -0500

    Initial Commit

---
+++ BookLive/BookLive/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BookLive.MvcApplication" Language="C#" %>
---
+++ BookLive/BookLive/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BookLive.Startup))]
+namespace BookLive
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
